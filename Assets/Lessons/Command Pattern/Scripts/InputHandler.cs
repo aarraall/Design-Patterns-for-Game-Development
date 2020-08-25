012dcd2cdb7c09b3025a5f7d1ca825b7b728a991
Aral Burak Doğan <aralburakdogan@gmail.com>
2020-08-25 13:06:05 +0300

    Command pattern handled

---
+++ Assets/Lessons/Command Pattern/Scripts/InputHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Lessons.Command_Pattern.Scripts
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public GameObject actor;
+        private Animator _animator;
+        private Command keySpace, keyW, keyP, keyF;
+        private int[] numbers = new int[3];
+
+        private void Start()
+        {
+            keySpace = new PerformJump();
+            keyW = new DoNothing();
+            keyP = new PerformPunch();
+            keyF = new PerformKick();
+            _animator = actor.GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                keySpace.Execute(_animator);
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                keyW.Execute(_animator);
+            }
+            else if (Input.GetKeyDown(KeyCode.P))
+            {
+                keyP.Execute(_animator);
+            }
+            else if (Input.GetKeyDown(KeyCode.F))
+            {
+                keyF.Execute(_animator);
+            }
+        }
+    }
+}
